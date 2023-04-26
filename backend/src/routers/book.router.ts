7776c1ed3227df30ee875e9ea579b77694e27cfe
Csanyi Mate <mcmatefr@gmail.com>
2023-04-26 22:45:35 +0200

    Connecting to Mongodb almost done

--- backend/src/routers/book.router.ts
+++ backend/src/routers/book.router.ts
@@ -1,18 +1,37 @@
 import {Router} from 'express';
 import { sample_books, sample_tags } from '../data';
+import asyncHandler from 'express-async-handler';
+import { BookModel } from '../models/book.model';
 
 const router = Router();
 
+router.get("/seed", asyncHandler(
+  async (req, res) => {
+    const booksCount = await BookModel.countDocuments();
+    if (booksCount> 0){
+      res.send("Seed is already done");
+      return;
+    }
+    
+    await BookModel.create(sample_books);
+    res.send("Seed Is Done!");
+  }
+))
 
-router.get("/", (req, res) => {
-    res.send(sample_books);
-})
+router.get("/",asyncHandler(
+  async (req, res) => {
+    const books = await BookModel.find();
+      res.send(books);
+  }
+))
 
-router.get("/search/:searchTerm", (req, res) => {
-    const searchTerm  = req.params.searchTerm;
-    const books = sample_books.filter(book => book.author.toLowerCase().includes(searchTerm.toLowerCase()))
+router.get("/search/:searchTerm", asyncHandler(
+  async (req, res) => {
+    const searchRegex = new RegExp(req.params.searchTerm, 'i');
+    const books = await BookModel.find({name: {$regex:searchRegex}});
     res.send(books);
-  })
+  }
+))
 
 router.get("/tags", (req, res) => {
     res.send(sample_tags);
