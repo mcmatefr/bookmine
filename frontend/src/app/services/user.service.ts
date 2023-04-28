e6eaf7ead07819fda0f6c6473184caee1c653163
Csanyi Mate <mcmatefr@gmail.com>
2023-04-28 23:04:46 +0200

    Finished

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -6,11 +6,12 @@ import { USER_LOGIN_URL } from '../shared/models/constants/urls';
 import { IUserLogin } from '../shared/models/interfaces/IUserLogin';
 import { User } from '../shared/models/User';
 
+const USER_KEY = 'User';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(new User());
+  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage());
   public userObservable:Observable<User>;
   constructor(private http:HttpClient) {
     this.userObservable = this.userSubject.asObservable();
@@ -20,6 +21,7 @@ export class UserService {
    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
     tap({
       next: (user) =>{
+        this.setUserToLocalStorage(user);
         this.userSubject.next(user);
 
       },
@@ -30,5 +32,20 @@ export class UserService {
    );
   }
 
+  logout(){
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload();
+  }
+
+  private setUserToLocalStorage(user:User){
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage():User{
+    const userJson = localStorage.getItem(USER_KEY);
+    if(userJson) return JSON.parse(userJson) as User;
+    return new User();
+  }
 }
 
